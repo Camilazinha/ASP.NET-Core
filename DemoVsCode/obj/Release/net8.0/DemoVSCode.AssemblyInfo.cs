3492cf9a2af7a75848393ca4704084f10f476db9
Camilazinha <caamiladepaula03@gmail.com>
2024-08-22 14:31:30 -0300

    teste

--- DemoVsCode/obj/Release/net8.0/DemoVSCode.AssemblyInfo.cs
+++ DemoVsCode/obj/Release/net8.0/DemoVSCode.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoVSCode")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b019dea5cea044f968effb88ea5416a0b91b9ada")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoVSCode")]
